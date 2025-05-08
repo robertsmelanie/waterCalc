de87bb62c12bfd7efde506f3c870c2fe055fe72a
Melanie Roberts <robertsmelaniecc@gmail.com>
2025-05-07 23:26:29 -0400

    cups

--- script.js
+++ script.js
@@ -10,5 +10,7 @@ function calcWater() {
     if (activity === "active") multiplier = 0.7;
     const ounces = (weight * multiplier).toFixed(1);
     const liters = (ounces * 0.0296).toFixed(1);
+    const cups = (ounces / 8).toFixed(1);
+    const gallons = (ounces / 128).toFixed(2);
     document.getElementById("output").innerText = `You should drink about ${ounces} oz (${liters} liters) of water daily.`;
 }
